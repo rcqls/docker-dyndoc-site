e4b62a3cc3a4548ca63806f28e9b966ec8558b38
rcqls <rdrouilh@gmail.com>
2019-11-24 10:28:55 +0100

    more to host

---
+++ scripts/install.sh
@@ -0,0 +1,22 @@
+install-julia-user() {
+	jl=$1
+	extra=$2
+	if [ "$extra" = "" ];then 
+		extra="0"
+	fi
+	juliabin=julia-${jl}.${extra}
+	juliatgz=${juliabin}-linux-x86_64.tar.gz
+
+	mkdir -p /home/ubuntu/tools/install/src
+	cd /home/ubuntu/tools/install/src
+
+	if ! [ -f  "${juliatgz}" ]; then
+ 		wget https://julialang-s3.julialang.org/bin/linux/x64/$jl/${juliatgz}
+		cd ..
+		tar xzvf src/${juliatgz}
+		echo "JULIA_RUBYLIB_PATH=$(/usr/bin/env ruby -e 'puts Dir[RbConfig::CONFIG["libdir"]+"/**/libruby*"].select{|e| e =~ /\.so$/}[0]')" >> ~/.bashrc
+	fi
+
+	cd /home/ubuntu/tools/bin 
+	ln -sf ../install/${juliabin}/bin/julia julia
+}
