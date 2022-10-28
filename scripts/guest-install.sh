995d1ae801e5819907c29abcf23c91c7816defb5
R cqls <rdrouilh@gmail.com>
2022-10-28 13:48:08 +0200

    add install julia to guest_install.sh

--- scripts/guest-install.sh
+++ scripts/guest-install.sh
@@ -55,27 +55,35 @@ user-export-rubylib() {
 	echo "export JULIA_RUBYLIB_PATH=$(/usr/bin/env ruby -e 'puts Dir[RbConfig::CONFIG["libdir"]+"/**/libruby*"].select{|e| e =~ /\.so$/}[0]')" >> $script
 }
 
-# install-julia-guest-user() {
-# 	jl=$1
-# 	extra=$2
-# 	if [ "$extra" = "" ];then 
-# 		extra="0"
-# 	fi
-# 	juliabin=julia-${jl}.${extra}
-# 	juliatgz=${juliabin}-linux-x86_64.tar.gz
+user-install-julia() {
+        jl=$1
+        extra=$2
+        if [ "$extra" = "" ];then 
+                extra="0"
+        fi
+        juliabin=julia-${jl}.${extra}
+        juliatgz=${juliabin}-linux-x86_64.tar.gz
 
-# 	mkdir -p /home/ubuntu/tools/install/src
-# 	cd /home/ubuntu/tools/install/src
+        mkdir -p /home/ubuntu/tools/install/src
+        cd /home/ubuntu/tools/install/src
 
-# 	if ! [ -f  "${juliatgz}" ]; then
-#  		wget https://julialang-s3.julialang.org/bin/linux/x64/$jl/${juliatgz}
-# 		cd ..
-# 		tar xzvf src/${juliatgz}
-# 	fi
+        if ! [ -f  "${juliatgz}" ]; then
+                wget https://julialang-s3.julialang.org/bin/linux/x64/$jl/${juliatgz}
+                cd ..
+                tar xzvf src/${juliatgz}
+        fi
 
-# 	cd /home/ubuntu/tools/bin 
-# 	ln -sf ../install/${juliabin}/bin/julia julia
-# }
+        cd /home/ubuntu/tools/bin 
+        ln -sf ../install/${juliabin}/bin/julia julia
+}
+
+user-dyndoc-jl() {
+        script=/home/ubuntu/tools/etc/dyndoc.yml
+        mkdir -p /home/ubuntu/tools/etc
+        echo "---" > $script
+        echo "cfg_dyn:" >> $script
+        echo "  langs: R,jl" >> $script
+}
 
 ## A appeler en début d'install
 root-install-begin() {
